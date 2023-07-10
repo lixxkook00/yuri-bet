1390b1f7ee5178328091b92ca1024d445ed3933f
linh <linhlinhkook00@gmail.com>
2023-07-10 20:20:15 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,84 @@
+const bind1 = document.querySelector.bind(document)
+const bind2 = document.querySelectorAll.bind(document)
+
+const toggleNavMobile = () => {
+    bind1('.soft-menu').classList.toggle('open')
+    bind1('.soft-menu__list').classList.toggle('open')
+}
+
+bind2('.soft-menu__item').forEach((element) => {
+  element.onclick = () => {
+    toggleNavMobile()
+  }
+})
+
+bind1('.scroll-to-top').onclick = () => {
+    window.scrollTo({
+        top: 0,
+        left: 100,
+        behavior: 'smooth'
+    });
+}
+
+// handle animation number
+function animateNumber(finalNumber, duration = 5000, startNumber = 0, callback) {
+    let currentNumber = startNumber
+    const interval = window.setInterval(updateNumber, 17)
+
+    function updateNumber() {
+        if (currentNumber >= finalNumber) {
+            clearInterval(interval)
+        } else {
+            let inc = Math.ceil(finalNumber / (duration / 17))
+            if (currentNumber + inc > finalNumber) {
+                currentNumber = finalNumber
+                clearInterval(interval)
+            } else {
+                currentNumber += inc
+            }
+            callback(currentNumber)
+        }
+    }
+}
+
+// loading animation
+window.addEventListener('load', (event) => {
+  bind1('.loading').style.display = "none"
+
+});
+
+
+// SLIDER
+var swiper = new Swiper(".swiper-l-banner", {
+    slidesPerView: 1,
+    centeredSlides: true,
+    spaceBetween: 0,
+    // effect: 'fade',
+    // auto
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+    },
+    loop: true,
+    loopedSlides: 50,
+    // speed: 1000,
+    // // initialSlide: 1,
+    // grabCursor: true,
+    // // navigation: {
+    // //     nextEl: ".swiper-new-roadmap-next",
+    // //     prevEl: ".swiper-new-roadmap-prev",
+    // // },
+    // breakpoints: {
+    //     // when window width is >= 480px
+    //     120: {
+    //         slidesPerView: 1,
+    //         spaceBetween: 20
+    //     },
+    //     // when window width is >= 640px
+    //    // when window width is >= 640px
+    //     740: {
+    //         slidesPerView: 1,
+    //         spaceBetween: 30
+    //     }
+    // },
+});
